cdbb443dc11e686863000c02e2ac65f101216e9b
moepapahlaing <moepapahlaing21@gmail.com>
2021-06-16 09:45:42 +0630

    initial

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>moefdf pdjfk  jfdkdjf lsdfjdkf kdfjslsdf dkfjkdfjssldfk df kdjfjdkff ;sldfjk slkdjf dfkjkdfjls fjdkflsfk dfkjsl</p>
+                <p>moefdf pdjfk  jfdkdjf lsdfjdkf kdfjslsdf dkfjkdfjssldfk df kdjfjdkff ;sldfjk slkdjf dfkjkdfjls fjdkflsfk dfkjsl</p>
+            </div>
+        </>
+    )
+}
+
+export default About
+
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>moefdf pdjfk  jfdkdjf lsdfjdkf kdfjslsdf dkfjkdfjssldfk df kdjfjdkff ;sldfjk slkdjf dfkjkdfjls fjdkflsfk dfkjsl</p>
+        <p className={styles.text}>moefdf pdjfk  jfdkdjf lsdfjdkf kdfjslsdf dkfjkdfjssldfk df kdjfjdkff ;sldfjk slkdjf dfkjkdfjls fjdkflsfk dfkjsl</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
+  )
+}
